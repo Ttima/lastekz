c9e47da085ca36eb39eb84a13004bda6882b7976
Ttima <tanjanett@mail.ru>
2017-07-05 23:31:38 +0300

    newsection

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,9 @@
 $('.slide-content').slick({
 	autoplay: true,
     autoplaySpeed: 1000,
-    arrows: false
+    arrows: true,
+    prevArrow: '<img class="slick-prev" src="../../prod/images/arrows1.png">',
+  nextArrow: '<img class="slick-next" src="../../prod/images/arrows.png">'
 });
 
 
